8e4ed433a7875d3e1ea5cf10068f8deab51ef77c
Rohan <rsrohan99@gmail.com>
2025-01-17 12:10:47 +0600

    added cmd line arguments

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import sys
 import asyncio
+import argparse
 
 from dotenv import load_dotenv
 
@@ -12,18 +13,27 @@ from agents.video_creator import PresenterVideoCreaterWorkflow
 
 async def main():
     load_dotenv()
+    parser = argparse.ArgumentParser(
+        description="Presenter - Create beautiful presentations using AI.",
+        usage="python run.py <topic> [--export-video] [-h]",
+    )
+    parser.add_argument(
+        "--export-video",
+        action="store_true",
+        help="Export a video of the presentation with voiceover",
+    )
+    args = parser.parse_args()
     llm = OpenAI(model="gpt-4o-mini")
     workflow = PresenterWorkflow(llm=llm, verbose=False, timeout=240.0)
     # draw_all_possible_flows(workflow, filename="workflow.html")
     topic = sys.argv[1]
-    # presentation_folder = await workflow.run(query=topic)
-    presentation_folder = "presentations/widevine"
-    # for i, slide in enumerate(result.slides):
-    #     print(f"{i+1}. Title: {slide.title}\nContent: {slide.atomic_core_idea}\n")
+    presentation_dir = await workflow.run(query=topic)
     video_creator_workflow = PresenterVideoCreaterWorkflow(
         model="eleven_flash_v2_5", voice="9BWtsMINqrJLrRacOk9x"
     )
-    await video_creator_workflow.run(presentation_dir=presentation_folder)
+    if args.export_video:
+        print("\n> Exporting video of the presentation with voiceover...\n")
+        await video_creator_workflow.run(presentation_dir=presentation_dir)
 
 
 if __name__ == "__main__":
